35197806947c56a7aa1b8b460709968719cd2146
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-01-23 18:23:32 -0300

    fix: add errorMessage and value from email and password

--- Inicio do projeto/dashboard/src/components/ModalLogin/index.vue
+++ Inicio do projeto/dashboard/src/components/ModalLogin/index.vue
@@ -1,9 +1,123 @@
 <template>
-  <h1>Modal do login</h1>
+ <div class="flex justify-between">
+   <h1 class="text-4xl font-black text-gray-800">Entre na sua conta</h1>
+ <button @click="close" class="text-4xl text-gray-600 focus:outline-none">
+  &times;
+ </button>
+ </div>
+
+ <div class="mt-16">
+   <form @submit.prevent="handleSubmit">
+     <label class="block">
+       <span class="text-lg font-medium text-gray-800">E-mail</span>
+       <input
+       v-model="state.email.value"
+       type="email"
+       :class="{'border-brand-danger': !!state.email.errorMessage}"
+       class="block
+        w-full
+        px-4
+        py-3
+        mt-1
+        text-lg
+        bg-gray-100
+        border-2
+        border-transparent
+        rounded"
+        placeholder="jane.dae@gmail.com"
+        >
+        <span
+        v-if="!!state.email.errorMessage"
+        class="block font-medium text-brand-danger">{{ state.email.errorMessage }}</span>
+     </label>
+
+       <label class="block mt-9">
+       <span class="text-lg font-medium text-gray-800">Senha</span>
+       <input
+       v-model="state.password.value"
+       type="password"
+       :class="{'border-brand-danger': !!state.password.errorMessage}"
+       class="block
+        w-full
+        px-4
+        py-3
+        mt-1
+        text-lg
+        bg-gray-100
+        border-2
+        border-transparent
+        rounded"
+        placeholder="jane.dae@gmail.com"
+        >
+        <span
+          v-if="!!state.password.errorMessage"
+          class="block font-medium text-brand-danger"
+          >
+          {{ state.password.errorMessage }}
+        </span>
+     </label>
+
+     <button
+      :disabled="state.isLoading"
+      :class="{'opacity-50': state.isLoading}"
+      class="
+        px-8
+        py-3
+        mt-10
+        text-2xl
+        font-bold
+        text-white
+        rounded-full
+        bg-brand-main
+        focus:outline-none
+        transition-all
+        duration-150
+        "
+      >
+      Entrar
+      </button>
+   </form>
+ </div>
 </template>
 
 <script>
+import { reactive } from 'vue'
+import { useField } from 'vee-validate'
+import useModal from '../../hooks/useModal'
+import { validateEmptyAndLength3, validateEmptyAndEmail } from '../../utils/validators'
+
 export default {
+  setup  () {
+    const modal = useModal()
+    const {
+      value: emailValue,
+      errorMessage: emailErrorMessage
+    } = useField('email', validateEmptyAndEmail)
+    const {
+      value: passwordValue,
+      errorMessage: passwordErrorMessage
+    } = useField('password', validateEmptyAndLength3)
+    const state = reactive({
+      hasErrors: false,
+      isLoading: false,
+      email: {
+        value: emailValue,
+        errorMessage: emailErrorMessage
+      },
+      password: {
+        value: passwordValue,
+        errorMessage: passwordErrorMessage
+      }
+    })
+
+    function handleSubmit () { }
+
+    return {
+      state,
+      close: modal.close,
+      handleSubmit
+    }
+  }
 
 }
 </script>
