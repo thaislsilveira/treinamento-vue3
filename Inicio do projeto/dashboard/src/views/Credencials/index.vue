15480ef70e17f5cd427093b9cc68cc1af17ac5a1
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-01-24 21:28:08 -0300

    feat: add ContentLoader

--- Inicio do projeto/dashboard/src/views/Credencials/index.vue
+++ Inicio do projeto/dashboard/src/views/Credencials/index.vue
@@ -14,8 +14,16 @@
       <p class="mt-10 text-lg text-gray-800 font-regular">
         Este aqui é a sua chave de api
       </p>
-      <div class="flex py-3 pl-5 mt-2 rounded justify-between items-center bg-brand-gray w-full lg:w-1/2">
-      <span>{{ store.User.currentUser.apiKey }}</span>
+
+      <content-loader
+        v-if="store.Global.isLoading || state.isLoading"
+        class="rounded"
+        width="600px"
+        height="50px"
+      />
+
+      <div v-else class="flex py-3 pl-5 mt-2 rounded justify-between items-center bg-brand-gray w-full lg:w-1/2">
+      <span>{{ store.User.currentUser.apikey[0] }}</span>
         <div class="flex ml-20 mr-5">
           <icon
             name="copy"
@@ -35,7 +43,15 @@
        <p class="mt-5 text-lg text-gray-800 font-regular">
         Coloque o script abaixo no seu site para começar a receber feedbacks
       </p>
-      <div class="py-3 pl-5 pr-20 mt-2 rounded bg-brand-gray w-full lg:w-2/3 overflow-x-scroll" >
+
+        <content-loader
+        v-if="store.Global.isLoading || state.isLoading"
+        class="rounded"
+        width="600px"
+        height="50px"
+      />
+
+      <div v-else class="py-3 pl-5 pr-20 mt-2 rounded bg-brand-gray w-full lg:w-2/3 overflow-x-scroll" >
         <pre>
           &lt;script src="https://thaislsilveira-feedbacker-widget.netlify.app?api_key={{store.User.currentUser.apiKey}}"&gt;&lt;/script&gt;"
         </pre>
@@ -46,17 +62,23 @@
 </template>
 
 <script>
+import { reactive } from 'vue'
 import HeaderLogged from '../../components/HeaderLogged'
+import ContentLoader from '../../components/ContentLoader'
 import Icon from '../../components/Icon'
 import useStore from '../../hooks/useStore'
 import palette from '../../../palette'
 
 export default {
-  components: { HeaderLogged, Icon },
+  components: { ContentLoader, HeaderLogged, Icon },
   setup () {
     const store = useStore()
+    const state = reactive({
+      isLoading: false
+    })
 
     return {
+      state,
       store,
       brandColors: palette.brand
     }
