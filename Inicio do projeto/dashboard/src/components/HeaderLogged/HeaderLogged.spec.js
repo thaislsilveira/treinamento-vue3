fd1e3f5f2a2ed7524ae080148e462c3476fb8a1f
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-01-30 13:28:51 -0300

    test: add test Headerlogged

---
+++ Inicio do projeto/dashboard/src/components/HeaderLogged/HeaderLogged.spec.js
@@ -0,0 +1,55 @@
+import { shallowMount } from '@vue/test-utils'
+import HeaderLogeed from '.'
+import { routes } from '../../router'
+
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory('/'),
+  routes
+})
+const mockStore = { currentUser: {} }
+jest.mock('../../hooks/useStore', () => {
+  return () => {
+    return mockStore
+  }
+})
+
+describe('<HeaderLogged />', () => {
+  it('should render header logged correctly', async () => {
+    router.push('/')
+    await router.isReady()
+    const wrapper = shallowMount(HeaderLogeed, {
+      global: {
+        plugins: [router]
+      }
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('should render 3 dots when there\'s not user logged', async () => {
+    router.push('/')
+    await router.isReady()
+    const wrapper = shallowMount(HeaderLogeed, {
+      global: {
+        plugins: [router]
+      }
+    })
+    const buttonLogout = wrapper.find('#logout-button')
+    expect(buttonLogout.text()).toBe('...')
+  })
+
+  it('should render user anem when there\'s user logged', async () => {
+    router.push('/')
+    await router.isReady()
+    mockStore.currentUser.name = 'Thais'
+    const wrapper = shallowMount(HeaderLogeed, {
+      global: {
+        plugins: [router]
+      }
+    })
+    const buttonLogout = wrapper.find('#logout-button')
+    expect(buttonLogout.text()).toBe('Thais (sair)')
+  })
+})
--- Inicio do projeto/dashboard/src/components/HeaderLogged/index.vue
+++ Inicio do projeto/dashboard/src/components/HeaderLogged/index.vue
@@ -11,7 +11,7 @@
        <li @click="() => router.push({ name: 'Feedbacks'})" class="px-6 py-2 mr-2 font-bold text-white rounded-full cursor-pointer focus:outline-none">
         Feedbacks
       </li>
-      <li @click="handleLogout" class="px-6 py-2 font-bold bg-white rounded-full cursor-pointer text-brand-main focus:outline-none">
+      <li  id="logout-button" @click="handleLogout" class="px-6 py-2 font-bold bg-white rounded-full cursor-pointer text-brand-main focus:outline-none">
         {{ logoutLabel }}
       </li>
     </ul>
