a3bed82c32e6ce40bb4eb0ee0c308172c1a32af1
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-01-30 22:15:14 -0300

    configure for netlify

--- dashboard/src/views/Feedbacks/Filters.vue
+++ dashboard/src/views/Feedbacks/Filters.vue
@@ -1,33 +1,33 @@
 <template>
-<div class="flex flex-col">
-  <h1 class="text-2xl font-regular text-brand-darkgray">
-    Filtros
-  </h1>
-  <ul class="flex flex-col mt-3 list-none">
-    <li
-    v-for="filter in state.filters"
-    :key="filter.label"
-    :class="{
-      'bg-gray-200 bg-opacity-50': filter.active}"
-      @click="() => handleSelect(filter)"
-      class="flex items-center justify-between px-4 py-1 rounded cursor-pointer"
+  <div class="flex flex-col">
+    <h1 class="text-2xl font-regular text-brand-darkgray">
+      Filtros
+    </h1>
+
+    <ul class="flex flex-col mt-3 list-none">
+      <li
+        v-for="filter in state.filters"
+        :key="filter.label"
+        :class="{
+          'bg-gray-200 bg-opacity-50': filter.active
+        }"
+        @click="() => handleSelect(filter)"
+        class="flex items-center justify-between px-4 py-1 rounded cursor-pointer"
       >
-      <div class=" flex items-center">
+        <div class="flex items-center">
+          <span
+            :class="filter.color.bg"
+            class="inline-block w-2 h-2 mr-2 rounded-full"/> {{ filter.label }}
+        </div>
         <span
-        :class="`bg-${filter.color}`"
-        class="inline-block w-2 h-2 mr-2 rounded-full"
-        />
-        {{filter.label}}
-      </div>
-      <span
-      :class="filter.active ? `text-${filter.color}` : 'text-brand-graydark'"
-      class="font-bold"
-      >
-        {{filter.amount}}
-      </span>
+          :class="filter.active ? filter.color.text : 'text-brand-graydark'"
+          class="font-bold"
+        >
+          {{ filter.amount }}
+        </span>
       </li>
-  </ul>
-</div>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -43,10 +43,10 @@ const LABELS = {
 }
 
 const COLORS = {
-  all: 'brand-info',
-  issue: 'brand-danger',
-  idea: 'brand-warning',
-  other: 'brand-graydark'
+  all: { text: 'text-brand-info', bg: 'bg-brand-info' },
+  issue: { text: 'text-brand-danger', bg: 'bg-brand-danger' },
+  idea: { text: 'text-brand-warning', bg: 'bg-brand-warning' },
+  other: { text: 'text-brand-graydark', bg: 'bg-brand-graydark' }
 }
 
 function applyFiltersStructure (summary) {
