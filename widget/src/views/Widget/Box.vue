3f901a010cc5757d924b911c12bdf987b951ac9e
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-02-10 16:29:42 -0300

    feat: add floating button

---
+++ widget/src/views/Widget/Box.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Box</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- widget/src/views/Widget/index.vue
+++ widget/src/views/Widget/index.vue
@@ -1,12 +1,49 @@
 <template>
-  <div class="fixed bottom-0 right-0">
-    widget
-  </div>
+   <teleport to="body">
+    <component
+      @open-box="handleOpenBox"
+      @close-box="handleCloseBox"
+      :is="state.component"
+    />
+  </teleport>
 
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent, reactive } from 'vue'
+import Standby from './Standby.vue'
+import Box from './Box.vue'
 
+type State = {
+  component: string;
 }
+
+interface SetupReturn {
+  state: State;
+  handleOpenBox(): void;
+  handleCloseBox(): void;
+}
+
+export default defineComponent({
+  components: { Standby, Box },
+  setup (): SetupReturn {
+    const state = reactive<State>({
+      component: 'Standby'
+    })
+
+    function handleOpenBox (): void {
+      state.component = 'Box'
+    }
+
+    function handleCloseBox (): void {
+      state.component = 'Standby'
+    }
+
+    return {
+      state,
+      handleOpenBox,
+      handleCloseBox
+    }
+  }
+})
 </script>
