73566ed972e826181fd12aaaa608204b8a32bb2e
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-02-11 15:06:19 -0300

    feat: add Wizard error state

--- widget/src/components/Wizard/index.vue
+++ widget/src/components/Wizard/index.vue
@@ -12,6 +12,7 @@ import { defineComponent } from 'vue'
 import SelectFeedbackType from './SelectFeedbackType.vue'
 import WriteAFeedback from './WriteAFeedback.vue'
 import Success from './Success.vue'
+import ErrorState from './Error.vue'
 import useStore from '@/hooks/store'
 import { StoreState, setFeedbackType } from '@/store'
 import useNavigation, { Navigation } from '../../hooks/navigation'
@@ -24,7 +25,7 @@ interface SetupReturn {
 }
 
 export default defineComponent({
-  components: { SelectFeedbackType, WriteAFeedback, Success },
+  components: { SelectFeedbackType, WriteAFeedback, Success, Error: ErrorState },
   setup (): SetupReturn {
     const store = useStore()
     const { next } = useNavigation()
