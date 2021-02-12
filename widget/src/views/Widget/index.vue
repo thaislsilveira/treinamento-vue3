8865a4e746243c2b51f6545eacb3fd100d302d18
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-02-12 09:57:57 -0300

    feat: add iframe in popup

--- widget/src/views/Widget/index.vue
+++ widget/src/views/Widget/index.vue
@@ -10,9 +10,10 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from 'vue'
+import { defineComponent, onMounted, reactive } from 'vue'
 import Standby from './Standby.vue'
 import Box from './Box.vue'
+import useIframeControl from '../../hooks/iframe'
 
 type State = {
   component: string;
@@ -27,15 +28,22 @@ interface SetupReturn {
 export default defineComponent({
   components: { Standby, Box },
   setup (): SetupReturn {
+    const iframe = useIframeControl()
     const state = reactive<State>({
       component: 'Standby'
     })
 
+    onMounted(() => {
+      iframe.updateCoreValuesOnStore()
+    })
+
     function handleOpenBox (): void {
+      iframe.notifyOpen()
       state.component = 'Box'
     }
 
     function handleCloseBox (): void {
+      iframe.notifyClose()
       state.component = 'Standby'
     }
 
